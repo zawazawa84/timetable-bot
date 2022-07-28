170d40b0c4de9f3fe55c6ce37bfb13ab6495e4e6
aizawa <taiki84taiki@gmail.com>
2022-07-28 13:56:55 +0900

    現在時刻に対応

--- main.js
+++ main.js
@@ -26,10 +26,10 @@ app.post("/webhook", async (req, res) => {
         messageText.indexOf("次") !== -1 &&
         messageText.indexOf("授業") !== -1
       ) {
-        const now = new Date("2022-7-15 12:00:00");
+        const now = new Date();
         const lesson = getNextLesson(now);
         if (lesson !== null) {
-          await replyMessage(`次の授業は、${lesson.name}です。教室は${lesson.place}`, replyToken);
+          await replyMessage(`次の授業は、${lesson.name}です。教室は${lesson.place}です。`, replyToken);
         } else {
           await replyMessage("次の授業はありません", replyToken);
         }
